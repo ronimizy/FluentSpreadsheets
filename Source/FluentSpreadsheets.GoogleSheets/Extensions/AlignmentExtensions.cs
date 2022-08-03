f82c174a94bee5604d43c13c2c61e657f3f76c56
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-03 14:16:28 +0300

    feat: add extensions

---
+++ Source/FluentSpreadsheets.GoogleSheets/Extensions/AlignmentExtensions.cs
@@ -0,0 +1,26 @@
+namespace FluentSpreadsheets.GoogleSheets.Extensions;
+
+internal static class AlignmentExtensions
+{
+    public static string ToGoogleSheetsAlignment(this VerticalAlignment alignment)
+    {
+        return alignment switch
+        {
+            VerticalAlignment.Top => "TOP",
+            VerticalAlignment.Center => "MIDDLE",
+            VerticalAlignment.Bottom => "BOTTOM",
+            _ => "VERTICAL_ALIGN_UNSPECIFIED"
+        };
+    }
+    
+    public static string ToGoogleSheetsAlignment(this HorizontalAlignment alignment)
+    {
+        return alignment switch
+        {
+            HorizontalAlignment.Leading => "LEFT",
+            HorizontalAlignment.Center => "CENTER",
+            HorizontalAlignment.Trailing => "RIGHT",
+            _ => "HORIZONTAL_ALIGN_UNSPECIFIED"
+        };
+    }
+}
