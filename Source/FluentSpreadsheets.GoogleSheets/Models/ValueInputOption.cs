7a839585ba3912894e464f8ceeab61cd13e646b9
Mihinator3000 <70411602+Mihinator3000@users.noreply.github.com>
2022-08-03 14:15:19 +0300

    feat: add sheets enum models

---
+++ Source/FluentSpreadsheets.GoogleSheets/Models/ValueInputOption.cs
@@ -0,0 +1,20 @@
+﻿namespace FluentSpreadsheets.GoogleSheets.Models;
+
+public readonly struct ValueInputOption
+{
+    private readonly string _value;
+
+    private ValueInputOption(string value)
+    {
+        _value = value;
+    }
+
+    public static ValueInputOption UserEntered => new("USER_ENTERED");
+    public static ValueInputOption Raw => new("RAW");
+
+    public static implicit operator string(ValueInputOption option)
+        => option.ToString();
+
+    public override string ToString()
+        => _value;
+}
