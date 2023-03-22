68519008abedec72ad99dbc2f9176e1013f013aa
ronimizy <ronimizy@outlook.com>
2023-03-22 11:00:27 +0300

    fix: text style generic methods

--- Source/FluentSpreadsheets/Extensions/ComponentTextExtensions.cs
+++ Source/FluentSpreadsheets/Extensions/ComponentTextExtensions.cs
@@ -1,32 +1,36 @@
 using System.Drawing;
 using FluentSpreadsheets.Styles.Text;
+using FluentSpreadsheets.Wrappables;
 
 namespace FluentSpreadsheets;
 
 public static class ComponentTextExtensions
 {
-    public static IComponent WithTextStyle(this IComponent component, TextStyle textStyle)
+    public static T WithTextStyle<T>(this T component, TextStyle textStyle) where T : IWrappable<T>
     {
         var style = textStyle.AsStyle();
-
         return component.WithStyleApplied(style);
     }
 
-    public static IComponent WithTextStyle(
-        this IComponent component,
+    public static T WithTextStyle<T>(
+        this T component,
         Color? textColor = null,
         TextKind? textKind = null,
         TextWrapping? wrapping = null)
+        where T : IWrappable<T>
     {
         return component.WithTextStyle(new TextStyle(textColor, textKind, wrapping));
     }
 
-    public static IComponent WithTextColor(this IComponent component, Color textColor)
+    public static T WithTextColor<T>(this T component, Color textColor) where T : IWrappable<T>
         => component.WithTextStyle(textColor: textColor);
 
-    public static IComponent WithTextKind(this IComponent component, TextKind textKind)
+    public static T WithTextKind<T>(this T component, TextKind textKind) where T : IWrappable<T>
         => component.WithTextStyle(textKind: textKind);
 
-    public static IComponent WithTextWrapping(this IComponent component, TextWrapping wrapping = TextWrapping.Wrap)
-        => component.WithTextStyle(wrapping: wrapping);
+    public static T WithTextWrapping<T>(this T component, TextWrapping wrapping = TextWrapping.Wrap)
+        where T : IWrappable<T>
+    {
+        return component.WithTextStyle(wrapping: wrapping);
+    }
 }
